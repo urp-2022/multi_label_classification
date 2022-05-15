af88bd05d66a9797ad06e8038d590bc13a48712f
bwj2800 <bwj2800@naver.com>
2022-05-15 21:45:40 +0900

    calculate mAP

--- test.py
+++ test.py
@@ -3,9 +3,12 @@ import torch.nn as nn
 from torchvision import models
 import torchvision.transforms as transforms
 import numpy as np
+import time
 
 from PIL import Image
 from datasets.loader import VOC
+
+from helper_functions import mAP, AverageMeter, CocoDetection
 
 ctx = "cuda" if torch.cuda.is_available() else "cpu"
 device = torch.device(ctx)
@@ -75,17 +78,81 @@ print("Accuracy: {}%".format(accuracy))'''
 from sklearn.metrics import average_precision_score, precision_recall_curve
 np.seterr(invalid='ignore')
 
-ap=np.empty((0,20), float)
+#ap=np.empty((0,20), float)
+
+batch_time = AverageMeter()
+prec = AverageMeter()
+rec = AverageMeter()
+mAP_meter = AverageMeter()
+
+tp, fp, fn, tn, count = 0, 0, 0, 0, 0
+preds = []
+targets = []
+
+end = time.time()
 
 with torch.no_grad():
-    for images, labels in test_loader:
-        images = images.to(device)
-        outputs = model(images)
-        outputs = torch.sigmoid(outputs).cpu()
-        a = average_precision_score(labels_np, outputs_np, average=None)
-        ap = np.append(ap, [a], axis=0)
-        #print(a)
+    for input, target in test_loader:
+        input = input.to(device)
+        output = model(input)
+        output = torch.sigmoid(output).cpu()
 
-print("\nMAP=\n")
-mAP=ap.mean(axis=0)
-print(mAP)
+        target = target
+        #target = target.max(dim=1)[0]
+
+        # for mAP calculation
+        preds.append(output.cpu())
+        targets.append(target.cpu())
+
+        # measure accuracy and record loss
+        pred = output.data.gt(0.5).long()
+        #pred = torch.round(output)
+
+        tp += (pred + target).eq(2).sum(dim=0)
+        fp += (pred - target).eq(1).sum(dim=0)
+        fn += (pred - target).eq(-1).sum(dim=0)
+        tn += (pred + target).eq(0).sum(dim=0)
+        count += input.size(0)
+
+        this_tp = (pred + target).eq(2).sum()
+        this_fp = (pred - target).eq(1).sum()
+        this_fn = (pred - target).eq(-1).sum()
+        this_tn = (pred + target).eq(0).sum()
+
+        this_prec = this_tp.float() / (
+            this_tp + this_fp).float() * 100.0 if this_tp + this_fp != 0 else 0.0
+        this_rec = this_tp.float() / (
+            this_tp + this_fn).float() * 100.0 if this_tp + this_fn != 0 else 0.0
+
+        prec.update(float(this_prec), input.size(0))
+        rec.update(float(this_rec), input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        p_c = [float(tp[i].float() / (tp[i] + fp[i]).float()) * 100.0 if tp[
+                                                                             i] > 0 else 0.0
+               for i in range(len(tp))]
+        r_c = [float(tp[i].float() / (tp[i] + fn[i]).float()) * 100.0 if tp[
+                                                                             i] > 0 else 0.0
+               for i in range(len(tp))]
+        f_c = [2 * p_c[i] * r_c[i] / (p_c[i] + r_c[i]) if tp[i] > 0 else 0.0 for
+               i in range(len(tp))]
+
+        mean_p_c = sum(p_c) / len(p_c)
+        mean_r_c = sum(r_c) / len(r_c)
+        mean_f_c = sum(f_c) / len(f_c)
+
+        p_o = tp.sum().float() / (tp + fp).sum().float() * 100.0
+        r_o = tp.sum().float() / (tp + fn).sum().float() * 100.0
+        f_o = 2 * p_o * r_o / (p_o + r_o)
+
+'''
+print(
+        '--------------------------------------------------------------------')
+print(' * P_C {:.2f} R_C {:.2f} F_C {:.2f} P_O {:.2f} R_O {:.2f} F_O {:.2f}'
+      .format(mean_p_c, mean_r_c, mean_f_c, p_o, r_o, f_o))'''
+
+mAP_score = mAP(torch.cat(targets).numpy(), torch.cat(preds).numpy())
+print("mAP score:", mAP_score)
